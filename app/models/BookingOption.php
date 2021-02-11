28a46d45427dc9657e6d2d2703bf4ce7bd0f5a56
Roula Rohban <roula.rohban@gmail.com>
2021-02-11 22:12:35 +0200

    Relations

--- app/models/BookingOption.php
+++ app/models/BookingOption.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class BookingOption extends Model
 {
-    //
+    protected $guarded = ['id','created_at','updated_at'];
+
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class);
+    }
 }
