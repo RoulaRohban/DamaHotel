a3328056b87030917283ae1ebc6b30891a4a8635
Roula Rohban <roula.rohban@gmail.com>
2021-02-11 22:04:50 +0200

    built database

---
+++ database/migrations/2021_02_11_123304_create_bookings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->date('arrival_date');
+            $table->date('departure_date');
+            $table->integer('persons_num');
+            $table->float('total_cost');
+            $table->enum('status',
+                ['pending','completed','rejected']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
